285650863ec1eabe0925af7a267cf3efde9851f1
Anjan Roy <hello@itzmeanjan.in>
2025-05-23 12:54:53 +0400

    Encode arbitrary length input data using RLNC - creates a coded piece
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- src/encoder.rs
+++ src/encoder.rs
@@ -1,4 +1,5 @@
 use crate::gf256::Gf256;
+use rand::Rng;
 
 pub const BOUNDARY_MARKER: u8 = 0x81;
 
@@ -23,7 +24,45 @@ impl Encoder {
         Encoder { data, piece_count }
     }
 
-    pub fn code() -> Vec<u8> {
-        todo!()
+    pub fn code<R: Rng + ?Sized>(&self, rng: &mut R) -> Vec<u8> {
+        let random_coding_vector = (0..self.piece_count)
+            .map(|_| rng.random())
+            .collect::<Vec<Gf256>>();
+
+        let piece_byte_len = self.data.len() / self.piece_count;
+
+        let coded_piece = self
+            .data
+            .chunks_exact(piece_byte_len)
+            .zip(&random_coding_vector)
+            .map(|(piece, &random_symbol)| {
+                piece
+                    .iter()
+                    .map(|&symbol| Gf256::new(symbol) * random_symbol)
+                    .collect::<Vec<Gf256>>()
+            })
+            .fold(vec![Gf256::default(); piece_byte_len], |mut acc, cur| {
+                acc.iter_mut().zip(cur).for_each(|(a, b)| {
+                    *a = *a + b;
+                });
+
+                acc
+            })
+            .iter()
+            .map(|symbol| symbol.get())
+            .collect::<Vec<u8>>();
+
+        let mut full_coded_piece = vec![0u8; self.piece_count + piece_byte_len];
+
+        full_coded_piece[..self.piece_count]
+            .iter_mut()
+            .enumerate()
+            .for_each(|(idx, symbol)| {
+                *symbol = random_coding_vector[idx].get();
+            });
+
+        full_coded_piece[self.piece_count..].copy_from_slice(&coded_piece);
+
+        full_coded_piece
     }
 }
