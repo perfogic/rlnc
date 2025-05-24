3d189196c06e3e7be7594655735b0734fad7cd93
Anjan Roy <hello@itzmeanjan.in>
2025-05-24 22:19:39 +0400

    Benchmark full RLNC encoder routine
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

---
+++ benches/full_rlnc_encoder.rs
@@ -0,0 +1,73 @@
+use divan;
+use rand::Rng;
+use rlnc::full::encoder::Encoder;
+use std::fmt::Debug;
+use std::time::Duration;
+
+fn main() {
+    divan::Divan::default().bytes_format(divan::counter::BytesFormat::Binary).main();
+}
+
+struct RLNCConfig {
+    data_byte_len: usize,
+    piece_count: usize,
+}
+
+fn bytes_to_human_readable(bytes: usize) -> String {
+    let units = ["B", "KB", "MB", "GB", "TB"];
+    let mut bytes = bytes as f64;
+    let mut unit_index = 0;
+
+    while bytes >= 1024.0 && unit_index < units.len() - 1 {
+        bytes /= 1024.0;
+        unit_index += 1;
+    }
+
+    format!("{:.2} {}", bytes, units[unit_index])
+}
+
+impl Debug for RLNCConfig {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&format!(
+            "Data: {}, Piece Count: {}",
+            &bytes_to_human_readable(self.data_byte_len),
+            self.piece_count
+        ))
+    }
+}
+
+const ARGS: &[RLNCConfig] = &[
+    RLNCConfig {
+        data_byte_len: 1usize << 10,
+        piece_count: 1usize << 5,
+    },
+    RLNCConfig {
+        data_byte_len: 1usize << 15,
+        piece_count: 1usize << 5,
+    },
+    RLNCConfig {
+        data_byte_len: 1usize << 20,
+        piece_count: 1usize << 10,
+    },
+    RLNCConfig {
+        data_byte_len: 1usize << 25,
+        piece_count: 1usize << 10,
+    },
+    RLNCConfig {
+        data_byte_len: 1usize << 30,
+        piece_count: 1usize << 15,
+    },
+];
+
+#[divan::bench(args = ARGS, max_time = Duration::from_secs(300), skip_ext_time = true)]
+fn encode(bencher: divan::Bencher, rlnc_config: &RLNCConfig) {
+    let mut rng = rand::rng();
+
+    let data = (0..rlnc_config.data_byte_len).map(|_| rng.random()).collect::<Vec<u8>>();
+    let (encoder, _) = Encoder::new(data, rlnc_config.piece_count);
+
+    bencher
+        .counter(divan::counter::BytesCount::new(rlnc_config.data_byte_len))
+        .with_inputs(|| rand::rng())
+        .bench_refs(|mut rng| encoder.code(divan::black_box(&mut rng)));
+}
