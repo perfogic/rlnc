d04a2987f80f065a86c68c7fba0484977bbc096a
Anjan Roy <hello@itzmeanjan.in>
2025-05-25 18:22:13 +0400

    Update benchmark parameters for RLNC recoding
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- benches/full_rlnc_recoder.rs
+++ benches/full_rlnc_recoder.rs
@@ -12,6 +12,7 @@ fn main() {
 struct RLNCConfig {
     data_byte_len: usize,
     piece_count: usize,
+    recoding_with_piece_count: usize,
 }
 
 fn bytes_to_human_readable(bytes: usize) -> String {
@@ -30,9 +31,10 @@ fn bytes_to_human_readable(bytes: usize) -> String {
 impl Debug for RLNCConfig {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(&format!(
-            "{} data splitted into {} pieces",
+            "{} data splitted into {} pieces, recoding with {} pieces",
             &bytes_to_human_readable(self.data_byte_len),
-            self.piece_count
+            self.piece_count,
+            self.recoding_with_piece_count
         ))
     }
 }
@@ -40,23 +42,28 @@ impl Debug for RLNCConfig {
 const ARGS: &[RLNCConfig] = &[
     RLNCConfig {
         data_byte_len: 1usize << 10,
-        piece_count: 1usize << 5,
+        piece_count: 1usize << 4,
+        recoding_with_piece_count: 1usize << 3,
     },
     RLNCConfig {
-        data_byte_len: 1usize << 15,
-        piece_count: 1usize << 5,
+        data_byte_len: 1usize << 14,
+        piece_count: 1usize << 6,
+        recoding_with_piece_count: 1usize << 5,
     },
     RLNCConfig {
-        data_byte_len: 1usize << 20,
+        data_byte_len: 1usize << 18,
+        piece_count: 1usize << 8,
+        recoding_with_piece_count: 1usize << 7,
+    },
+    RLNCConfig {
+        data_byte_len: 1usize << 22,
         piece_count: 1usize << 10,
+        recoding_with_piece_count: 1usize << 9,
     },
     RLNCConfig {
-        data_byte_len: 1usize << 25,
-        piece_count: 1usize << 10,
-    },
-    RLNCConfig {
-        data_byte_len: 1usize << 30,
-        piece_count: 1usize << 15,
+        data_byte_len: 1usize << 26,
+        piece_count: 1usize << 12,
+        recoding_with_piece_count: 1usize << 11,
     },
 ];
 
@@ -66,14 +73,12 @@ fn recode(bencher: divan::Bencher, rlnc_config: &RLNCConfig) {
     let data = (0..rlnc_config.data_byte_len).map(|_| rng.random()).collect::<Vec<u8>>();
 
     let (encoder, piece_byte_len) = Encoder::new(data, rlnc_config.piece_count);
-
-    let num_pieces_to_recode_with = rlnc_config.piece_count / 2;
-    let coded_pieces = (0..num_pieces_to_recode_with)
+    let coded_pieces = (0..rlnc_config.recoding_with_piece_count)
         .flat_map(|_| encoder.code(&mut rng).unwrap())
         .collect::<Vec<u8>>();
 
     bencher
-        .counter(divan::counter::BytesCount::new(coded_pieces.len()))
+        .counter(divan::counter::BytesCount::new(rlnc_config.recoding_with_piece_count + coded_pieces.len()))
         .with_inputs(|| {
             let rng = rand::rng();
             let recoder = Recoder::new(coded_pieces.clone(), piece_byte_len, rlnc_config.piece_count).unwrap();
